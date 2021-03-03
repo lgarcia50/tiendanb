b5456efd8c8fb510dba319ef5cf0d4079d11c3d9
Lucia <lgarcia@eepmad.com>
2021-03-03 10:51:54 +0100

    1º prueba en neatbeans

---
+++ src/main/java/com/mycompany/tienda/ArticuloCarrito.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tienda;
+
+/**
+ *
+ * @author anaga
+ */
+public class ArticuloCarrito {
+     Articulo elemento;
+	 int cantidad;
+	
+	 public ArticuloCarrito() {}
+	 public ArticuloCarrito(Articulo articulo,int cantidad) {
+		 this.elemento=articulo;
+		 this.cantidad=cantidad;
+	 }
+	 @Override
+	 public String toString() {
+		 return this.elemento +"x"+this.cantidad+"\n";
+		 
+	 }
+}
